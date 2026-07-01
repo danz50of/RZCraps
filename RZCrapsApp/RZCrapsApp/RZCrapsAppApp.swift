df1ea4faf8c018f94b413842ab56080ed99ab948
Dan Zabinski <dan@dzabinski.com>
2026-06-30 22:16:53 -0500

    Initial scaffold

---
+++ RZCrapsApp/RZCrapsApp/RZCrapsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RZCrapsAppApp.swift
+//  RZCrapsApp
+//
+//  Created by Dan Zabinski on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct RZCrapsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
